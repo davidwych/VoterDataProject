e6d1e9969a1f0926a6ad2aa32487e8da081f548a
davidwych <davidwych@Davids-MacBook-Air.local>
2018-10-10 23:48:12 -0700

    Cleaning up Voters.py

--- Voters.py
+++ Voters.py
@@ -7,15 +7,29 @@ from polls import polls
 
 #USEFUL FUNCTIONS:
 def convert_to_percentages(array, weighted=False, sanity_check=False):
-    """
-    Converts an array of counts to percentages of the total
+    """Converts an array of counts to percentages of the total
 
-    ### inputs ###
-    array (list, numpy array) -- array of counts
+    Parameters
+    ----------
+    array : (list, numpy array)
+        array of counts
+        (len 1 for unweighted, len 2 for weighted)
 
-    ### outputs ###
-    unique_elements (list) -- unique elements from array
-    percs (numpy array) -- percentages for each element
+    Args
+    ----
+    weights : (bool)
+        True -- weighted (weighted by demographic factors)
+        False -- unweighted
+    sanity_check : (bool)
+        True -- perform sanity check (percentages at add to 1.0)
+        False -- trust the universe
+
+    Returns
+    -------
+    unique_elements : (list)
+        unique elements from array
+    percs : (numpy array)
+        percentages for each element
     """
     if not weighted:
         unique_elements = list(set(array))
@@ -34,7 +48,6 @@ def convert_to_percentages(array, weighted=False, sanity_check=False):
         total = np.sum(counts)
         percs = counts/total
 
-    #Sanity checking
     def sanity_checker():
         if np.sum(percs) == 1.0:
             print("Sanity confirmed!")
@@ -47,7 +60,21 @@ def convert_to_percentages(array, weighted=False, sanity_check=False):
 
 #VOTER class
 class Voters:
-    """Class for working with YouGov VOTER Study poll"""
+    """Class for working with YouGov VOTER Study Group data
+
+    Usage::
+        >>> all_voters = Voters()
+        >>> specific_voters = Voters().get_voters(column_label="<column_label>",
+                                                selection="<answer id>")
+        >>> specific_column = Voters().get_column(column_label="<column_label>")
+
+        To get access to the dataframe itself:
+        >>> voter_instance.data
+
+    :param data: largely unecessary to worry about. Loads the entire
+    YouGov dataset if initialized on its own, and copies the DataFrame
+    from other instances if manipulating with the function below
+    """
     def __init__(self, data='./VOTER_Survey_December16_Release1.csv'):
         if isinstance(data, str):
             self.data = pd.read_csv(data)
@@ -55,21 +82,75 @@ class Voters:
             self.data = data
 
     def __add__(self, other):
+        """Concatenate the dataframes of two Voters instances"""
         return Voters(data=pd.concat([self.data, other.data], axis=1))
 
     def __sub__(self, other):
+        """Subtracting the columns of dataframes of two Voters instances"""
         return Voters(data=self.data.drop(other.data.columns, axis=1))
 
     def get_column(self, column_label):
+        """Get data from one specific column in the dataframe
+
+        Parameters
+        ----------
+        column_label : (str)
+            label for the column in the dataframe
+
+        Returns
+        -------
+        Voters() instance : (class instance)
+
+        Usage::
+            To select the column for 2016 Presidential Election vote
+            >>> Voters().get_column(column_label="presvote16post_2016")
+
+        """
         column = pd.DataFrame(self.data[column_label].fillna(value=-1.0))
         return Voters(data=column)
 
     def get_voters(self, column_label, selection):
+        """Get dataframe with voters who match an answer id in a column
+
+        Parameters
+        ----------
+        column_label : (str)
+            label for the column in the dataframe
+        selection : (str)
+            answer index for specific response
+
+        Returns
+        -------
+        Voters() instance : (class instance)
+
+        Usage::
+            To get dataframe of Hillary voters in 2016 Presidential Election
+            >>> Voters().get_voters(column_label="presvote16post_2016",
+                                    selection=1)
+
+        """
         voters = self.data[:][self.data[column_label] == selection].fillna(value=-1.0)
         voters = voters.drop(column_label, axis=1)
         return Voters(data=voters)
 
-    def extract(self, selection="", weighted=False):
+    def _extract(self, selection="", weighted=False):
+        """Extracts a numpy array for input to plot_percentages()
+
+        Args
+        ----
+        selection : (str) (optional)
+            extract a particular column of the dataframe
+            (otherwise, Voters() column instance is automatically extracted)
+        weighted : (bool) (optional)
+            True -- outputs both the data from the column and the weights
+                    column for the voters in the dataframe
+            False -- just extract the data from the column, no weighting
+
+        Returns
+        -------
+        numpy.ndarray (len 1 if weighted=False, len 2 if weighted=True)
+
+        """
         if selection == "":
             if not weighted:
                 return np.array(self.data[self.data.columns[0]])
@@ -83,18 +164,47 @@ class Voters:
 
     def plot_percentages(self, selection="", rotate_labels=True, weighted=True,
                          bar_labels=True, ft=False, show_not_sure=False):
+        """Plot bar chart of percentages of answers from the column on interest
+        in the dataframe
 
+        Parameters
+        ----------
+        selection : (str) (optional)
+            label for the column in the dataframe
+            (if not specified, the column of the dataframe
+            will be automatically plotted)
+        rotate_labels : (bool) (optional)
+            rotate the labels of the bars (if True)
+        weighted : (bool) (optional)
+            weight the data w.r.t demographics (if True)
+        bar_labels : (bool) (optional)
+            label the bars with their associated percentages
+        ft : (bool) (optional)
+            set to True if plotting a "Feelings Thermometer" question
+        show_not_sure : (bool) (optional)
+            show the percentages of voters who answered "not sure" (if True)
+
+        Returns
+        -------
+        Bar chart matplotlib plot
+
+        Usage::
+            Plot 2016 Presidential Election responses for all voters, weighted
+            >>> Voters().plot_percentages(column_label="presvote16post_2016",
+                                          rotate_labels=True, weighted=True)
+
+        """
         if selection == "":
             if not weighted:
-                opts, percs = convert_to_percentages(self.extract())
+                opts, percs = convert_to_percentages(self._extract())
             else:
-                opts, percs = convert_to_percentages(self.extract(weighted=True), weighted=True)
+                opts, percs = convert_to_percentages(self._extract(weighted=True), weighted=True)
             selection = self.data.columns[0]
         else:
             if not weighted:
-                opts, percs = convert_to_percentages(self.extract(selection=selection))
+                opts, percs = convert_to_percentages(self._extract(selection=selection))
             else:
-                opts, percs = convert_to_percentages(self.extract(selection=selection, weighted=True), weighted=True)
+                opts, percs = convert_to_percentages(self._extract(selection=selection, weighted=True), weighted=True)
 
         opts = np.array(opts)
         nans = []
